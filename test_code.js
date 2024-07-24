830b8132605e6e2641a3f39227dac43d6a19ca36
Piyush1869 <102930248+Piyush1869@users.noreply.github.com>
2024-07-24 23:57:48 +0530

    test code update

---
+++ test_code.js
@@ -0,0 +1,23 @@
+function form_unhide(){
+    const loadingDiv = document.querySelector('#test_code');
+    loadingDiv.style.display = 'block';
+
+    
+}
+
+
+
+
+
+
+function testcode(){
+    var x = document.getElementById("code").value;
+    let code = "bio-test0025"
+    let test_link = "https://docs.google.com/forms/d/e/1FAIpQLSeo0rVXGy8Oc5lhWO_mXdnp6oSpP4JqTVjvtqJW_s5J2ttmgw/viewform?usp=sf_link"
+    if (x == code) {
+        window.open(test_link, "_blank");
+        }
+        else {
+            alert("Wrong code! Try again.")
+            }
+}
