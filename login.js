edff7e967fb9fb99f8551fc172cca180d5c8a591
Piyush1869 <102930248+Piyush1869@users.noreply.github.com>
2024-08-07 01:43:09 +0530

    aad login event

---
+++ login.js
@@ -0,0 +1,85 @@
+// login.js
+   document.getElementById("root").innerHTML=`<center>
+      <div id="form_cont">
+       <h2> login by usear id given by your teacher</h2> <br><br>
+      <form id="myForm" action="" method="post" onsubmit="login(); return false;">
+        <input id="usearname" type="text" value="usear_name" onfocus="if(this.value === 'usear_name'){this.value = '';}
+       " >
+        
+        <br><br><br>
+        <input type="submit" id="submit" value="submit">
+      </form>
+      <div id="login_status"></div></div></center>`
+
+
+
+
+
+
+
+
+
+
+
+function login() {
+    let usearname = document.getElementById("usearname").value;
+    console.log("Username:", usearname);
+    // You can now use the username to authenticate the user
+    // For example, you can check if the username exists in a database or an object
+    let users = {
+      "avi09": { class: 9, subject: "PCB" },
+      "ansh": { class: 10, subject: "PCM" },
+      "avinashi": { class: "English", subject: "English" }
+    };
+  
+    if (usearname in users) {
+      let userData = { username: usearname, className: users[usearname].class, subject: users[usearname].subject };
+      // Send data to tution.js
+      tution.handleUserData(userData);
+      document.getElementById("login_status").innerHTML = "Login successful!";
+     
+    } else {
+      document.getElementById("login_status").innerHTML = "Username not found!";
+    }
+  }
+
+
+
+
+
+
+// // Create an object to store usernames and classes
+// let users = {
+//     "avi09": "Math",
+//     "ansh": "Science",
+//     "avinashi": "English"
+//   };
+  
+//   // Function to check if a user exists
+//   function checkUser(username) {
+//     if (users[username]) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+  
+//   // Function to login
+//   function login(username) {
+//     console.log(username);
+//     if (checkUser(username)) {
+//       // Send data to tution.js
+//       let userData = { username: username, className: users[username] };
+//       // Assuming you have a function in tution.js to handle the data
+//       tution.handleUserData(userData);
+//       alert("Successful login!");
+//     } else {
+//       alert("Username not found!");
+//     }
+//   }
+  
+//   // Login attempt
+//   login("avi09"); // Should alert "Successful login!"
+//   login("ansh"); // Should alert "Successful login!"
+//   login("avinashi"); // Should alert "Successful login!"
+//   login("unknownUser"); // Should alert "Username not found!"
---
+++ tution.js
@@ -0,0 +1,211 @@
+// tution.js
+
+
+class Tution {
+   timmer (data){
+    setTimeout(() => {
+        document.getElementById("root").innerHTML=data;
+      }, 2000);
+  }
+     root = document.getElementById("root").innerHTML
+    handleUserData(userData) {
+      console.log("Received user data:", userData);
+      // You can access the username and className here
+      let username = userData.username;
+    let className = userData.className;
+    let subject = userData.subject;
+
+    // Log all the data
+      console.log(username);
+      console.log(className);
+      console.log(typeof(className));
+      console.log(subject);
+      console.log(typeof(subject));
+
+      document.getElementById("root").innerHTML = `
+        <center><div id="form_cont">  <h1> login sucessfull ${name(username)} loding.............</h1></div></center>
+         `;
+       // aad delay 
+       setTimeout(() => {
+        document.getElementById("root").innerHTML=`<center><div id="form_cont">  <h1> redirecting to webpage  </h1>`;
+      }, 2000);
+     
+  
+
+
+      
+      setTimeout(() => {
+        
+         // Redirect to home.html
+      //  window.location.href = "./home.html";
+
+     
+
+    //Render the navigation bar
+    // Render the navigation bar
+  if (className === 9) {
+    const navBar = document.getElementById("root");
+    navBar.innerHTML = `
+    <nav>  
+    <ul>
+        <li><a href="#" onclick="renderBio9()">Bio</a></li>
+        <li><a href="#" onclick="renderPhysic9()">Physic</a></li>
+        <li><a href="#" onclick="renderChemistry9()">Chemistry</a></li>
+        <li><a href="#" onclick="renderTest9()">test</a></li>
+        <li><a href="./home.html">about me   </a></li>
+        </ul>
+      </nav>
+       <div id="content">
+       <h1> ${name(username)} go and check all subject  data </h1>
+       </div>
+    `;
+    document.body.appendChild(navBar);
+  }
+}, 3000);
+
+    
+    }
+
+}
+
+
+
+
+// Define the render methods for each subject
+function renderPhysic9() {
+  document.getElementById("content").innerHTML = `<h1>Physics</h1>
+    <div class="heading">Curious Point - Physics</div>
+    
+    <div >
+    <p>Welcome to the physics page!</p>
+    <p>Here you will find all the physics related content.</p>
+
+
+    <p class="chapter_pdf_class9">
+        class 9 chapter wise  pdf link <br>
+        </p>
+        <span class="star"><u>*click on chapter name text to open pdf if psd is not open on one click plz click again  *</u></span>
+        <p class="chapter_pdf_class10"><ul>
+  <li class="link"><a class="link_pdf" href="./ALL_STUDY_MATERIAL/motion in 1D.pdf" onclick="openPdf(this.href)">Motion in 1D  </a></li>
+  <li class="link"><a class="link_pdf" href="./ALL_STUDY_MATERIAL/LAWS OF MOTION.pdf" onclick="openPdf(this.href)"> Law of motion </a></li>
+</ul></p>
+   <div class="loading" id="loading">Loading...</div>
+        <iframe name="pdf_frame" src="" frameborder="0" width="100%" height="500"></iframe>
+  
+       
+        `;
+}
+
+function renderChemistry9() {
+  document.getElementById("content").innerHTML= `
+    <h1>Chemistry</h1>
+    <p class="chapter_pdf_class9">
+        class 9 chapter wise  pdf link <br>
+        </p>
+        <span class="star"><u>*click on chapter name text to open pdf if psd is not open on one click plz click again  *</u></span>
+        <p class="chapter_pdf_class10"><ul>
+            <li class="link"><a class="link_pdf" href="./ALL_STUDY_MATERIAL/water.pdf"  >water  </a></li>
+            <!-- <li class="link"><a class="link_pdf" href="./ALL_STUDY_MATERIAL/cell the fundamental unit of life.pdf"  > cell</a> -->
+          </ul></p>
+        
+        
+        <div class="loading" id="loading">Loading...</div>
+        <iframe name="pdf_frame" src="" frameborder="0" width="100%" height="500"></iframe>
+        
+  `;
+}
+
+function renderBio9() {
+  document.getElementById("content").innerHTML = `<h1>Biology</h1><div class="heading">Curious Point - Biology</div>
+    <p>Welcome to the biology page!</p>
+    <p class="chapter_pdf_class9">
+        class 9 chapter pdf link <br>
+        </p>
+        <span class="star"><u>*click on chapter name text to open pdf if psd is not open on one click plz click again  *</u></span>
+        <p class="chapter_pdf_class10"><ul>
+            <li class="link"><a class="link_pdf" href="./ALL_STUDY_MATERIAL/polination and fertilization.pdf"  >polination and fertilization pdf </a></li>
+            <li class="link"><a class="link_pdf" href="./ALL_STUDY_MATERIAL/cell the fundamental unit of life.pdf"  > cell</a>
+          </ul></p>
+        <div class="loading" id="loading">Loading...</div>
+        <iframe name="pdf_frame" src="" frameborder="0" width="100%" height="500"></iframe>
+  
+        <div class="loading" id="loading">Loading...</div>
+        <iframe name="pdf_frame" src="" frameborder="0" width="100%" height="500"></iframe>
+       
+        `;
+}
+
+function name(usear){
+  
+  if ( usear === "avi09"){
+
+    return "welcome Avinashi ";
+  }
+  else if (usear === ansh){
+    return "welcome Ansh";
+  }
+}
+
+
+
+function openPdf(link) {
+  // Get the iframe element
+  const iframe = document.getElementsByName("pdf_frame")[0];
+  
+  // Set the src attribute of the iframe to the clicked link
+  iframe.src = link;
+  
+  // Show the loading div
+  document.getElementById("loading").style.display = "block";
+  
+  // Hide the loading div after 2 seconds (assuming the pdf will be loaded by then)
+  setTimeout(() => {
+    document.getElementById("loading").style.display = "none";
+  }, 2000);
+}
+
+// Add an onclick event to the chapter links
+const links = document.getElementsByClassName("link_pdf");
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function() {
+    openPdf(this.href);
+  });
+}
+
+function renderTest9(){
+  document.getElementById("content").innerHTML =`
+  <br><br>
+  <div class="header" >WELCOME TO TEST PAGE </div><br><br>
+   <p class="test">
+    ACTIVE TEST LINK HERE :<br>
+    chemistory test chapter name {chemical change and reaction ; water } :<br><br>  
+    <a class="link" onclick="form_unhide()" >click me </a>
+    
+  </p>
+  <div class="test_code" id="test_code">
+   <center><form id="myForm" action="" method="post" onsubmit="testcode(); return false;">
+    <lable for="code">enter your test code </lable>
+    <input type="text" name="code" id="code" placeholder="enter your code">
+    <input type="submit" value="Submit">
+   </form></center> 
+  </div>
+  
+  
+  <br><br>
+    <p><H1>TEST RESULT </H1>
+     after verification you can see your result here 
+    </p>
+
+  `
+}
+
+
+
+
+
+  
+  // Create an instance of the Tution class
+  let tution = new Tution();
+  
+  // You can also use the data to make an API call or perform some other action
+ 
