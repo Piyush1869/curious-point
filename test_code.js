7bbebdb74cfe5845b5960d54c93f4020d0cbd7cf
Piyush1869 <102930248+Piyush1869@users.noreply.github.com>
2024-08-20 18:26:32 +0530

    test updated

--- test_code.js
+++ test_code.js
@@ -15,8 +15,11 @@ function testcode(){
     let code = "bio-test0025"
     let code2=  "PHY-02"
     let test_link = "https://docs.google.com/forms/d/e/1FAIpQLSeo0rVXGy8Oc5lhWO_mXdnp6oSpP4JqTVjvtqJW_s5J2ttmgw/viewform?usp=sf_link"
+    let test_link2 = "https://forms.gle/njC4qxPq5fU4Ah5s7"
     if (x == code) {
         window.open(test_link, "_blank");
+        }else if(x===code2){
+            window.open(test_link2, "_blank");
         }
         else {
             alert("Wrong code! Try again.")
