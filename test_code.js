b45813a219efbb8db053687fade5b538d1295225
Piyush1869 <102930248+Piyush1869@users.noreply.github.com>
2024-08-20 18:21:59 +0530

    partial

--- test_code.js
+++ test_code.js
@@ -13,6 +13,7 @@ function form_unhide(){
 function testcode(){
     var x = document.getElementById("code").value;
     let code = "bio-test0025"
+    let code2=  "PHY-02"
     let test_link = "https://docs.google.com/forms/d/e/1FAIpQLSeo0rVXGy8Oc5lhWO_mXdnp6oSpP4JqTVjvtqJW_s5J2ttmgw/viewform?usp=sf_link"
     if (x == code) {
         window.open(test_link, "_blank");
