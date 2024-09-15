e456f1895a8add74229124bc4bed58eebddab5e6
Piyush1869 <102930248+Piyush1869@users.noreply.github.com>
2024-09-15 23:47:29 +0530

    hygine test

--- test_code.js
+++ test_code.js
@@ -15,16 +15,21 @@ function testcode(){
     let code = "bio-test0025"
     let code2=  "PHY-02"
     let bio_test = "bio-test006"
+    let hygine = "bio-test8856"
      
     let test_link = "https://forms.gle/meVDv4657uPBsyFZ9"
     let test_link2 = "https://forms.gle/njC4qxPq5fU4Ah5s7"
     let test_link3 = "https://forms.gle/FnxdZXjwbPRcNW328"
+    let higine = "https://forms.gle/oaVz4xcEWvKiaoyL8"
+
     if (x == code) {
         window.open(test_link, "_blank");
         }else if(x===code2){
             window.open(test_link2, "_blank");
         }else if(x===bio_test) {
             window.open(test_link3, "_blank");
+            }else if(x==hygine){
+                window.open(higine, "_blank");
             }
         else {
             alert("Wrong code! Try again.")
